9e60eadec28cd3a3e69eeff61b2e035d306db153
anhnd <anhnd012@gmail.com>
2026-06-14 10:06:46 +0700

    add project

---
+++ src/test/java/com/appsdeveloperblog/ws/urlshorten/UrlShortenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.ws.urlshorten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
