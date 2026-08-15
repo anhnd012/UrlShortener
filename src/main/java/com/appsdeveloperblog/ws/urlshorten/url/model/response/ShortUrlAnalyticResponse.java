4d8edbed739ecaa672f8624922e887738be0940e
anhnd <anhnd012@gmail.com>
2026-08-15 07:38:39 +0700

    clean code

--- src/main/java/com/appsdeveloperblog/ws/urlshorten/url/model/response/ShortUrlAnalyticResponse.java
+++ src/main/java/com/appsdeveloperblog/ws/urlshorten/url/model/response/ShortUrlAnalyticResponse.java
@@ -1,21 +1,19 @@
 package com.appsdeveloperblog.ws.urlshorten.url.model.response;
 
 import com.appsdeveloperblog.ws.urlshorten.url.entity.ShortUrl;
+import java.util.UUID;
 import lombok.Data;
 import tools.jackson.databind.PropertyNamingStrategies;
 import tools.jackson.databind.annotation.JsonNaming;
 
-import java.util.UUID;
-
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ShortUrlAnalyticResponse {
-    private UUID shortUrlId;
-    private Long numberOfClicks;
+  private UUID shortUrlId;
+  private Long numberOfClicks;
 
-    public ShortUrlAnalyticResponse(ShortUrl url) {
-        this.shortUrlId = url.getId();
-        this.numberOfClicks = url.getNumberOfClicks();
-
-    }
+  public ShortUrlAnalyticResponse(ShortUrl url) {
+    this.shortUrlId = url.getId();
+    this.numberOfClicks = url.getNumberOfClicks();
+  }
 }
